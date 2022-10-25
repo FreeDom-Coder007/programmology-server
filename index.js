66c1ee752d0e5145e1804bb08db339a3a9710c58
FreeDom-Coder-007 <afzalsharif623@gmail.com>
2022-10-25 13:45:55 +0600

    created courses data

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+
+const courses = require('./Data/Courses.json')
+
+app.get('/', ( req,res ) => {
+   res.send('Coures Api Running')
+})
+app.get('/courses', (req,res) => {
+    res.send(courses)
+})
+
+app.listen(port, () => {
+    console.log('Programmology Srever in running on:', port)
+})
