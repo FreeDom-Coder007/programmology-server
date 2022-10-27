ab0b910e561c7c10b09115b57701028081d4160f
FreeDom-Coder-007 <afzalsharif623@gmail.com>
2022-10-27 19:15:32 +0600

    all data added

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ const courses = require('./Data/Courses.json')
 app.get('/', ( req,res ) => {
    res.send('Coures Api Running')
 })
+
 app.get('/courses', (req,res) => {
     res.send(courses)
 })
+app.get('/courses/:id', (req,res) => {
+    const id = req.params.id
+    const selectedCourse = courses.find(course => course.id == id)
+    res.send(selectedCourse)
+})
 
 app.listen(port, () => {
-    console.log(`Programmology Srever in running on:`, port)
+    console.log('Programmology Srever in running on:', port)
 })
 
 module.exports = app;
